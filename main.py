104d50f9b3ea8c6a2f2797bbb42d7633c0d2cbab
Raihan <17radf@gmail.com>
2025-01-10 22:15:18 +0700

    feat: add necessary package

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 def main():
     print("Hello from ems-model!")
 
