34b80425ef8411e42a89697c99981c6167deba9f
Raihan <17radf@gmail.com>
2025-01-10 22:33:07 +0700

    feat: add function to load csv

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def load_data(file_path):
+    """
+    Load the data from .csv file (must be a csv)
+    """
+    try:
+        df = pd.read_csv(file_path)
+        print("CSV Loaded, Dataset shape:", df.shape)
+        return df
+    except FileNotFoundError:
+        print(f"Error: CSV File {file_path} not found!")
+        return None
+
 def main():
     print("Hello from ems-model!")
+    load_data("ems_data.csv") # load from csv file
 
 if __name__ == "__main__":
     main()
